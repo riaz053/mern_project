80b01aa54ecf625827966156de0fc06d04946eb6
riaz_053 <riaz0907053@gmail.com>
2024-04-23 12:56:16 +0600

    done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/article" element={<Article />} />
+        <Route path="/article/:name" element={<Article />} />
         <Route path="/article-list" element={<Articlelist />} />
         </Routes>
       </div>
--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import {useParams} from 'react-router-dom';
+import articlecontent from './article-contetn';
 
 export default function Article() {
+  const {name} = useParams();
+  const article = articlecontent.find((article) => article.name === name);
+  if(!article) return <div>Article does not Exists</div>
   return (
-    <div>
+    <div> 
         <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
-            This is Article. Need to Check.
+            {article.title}
         </h1>
+        {article.content.map((paragraph, index)=> (
+          <p className='mx-auto leading-relaxed text-base mb-4' key={index}>
+            {paragraph}
+          </p>
+        ))}
     </div>
   )
 }
